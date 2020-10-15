6a6e24f72a0aa993be25d4b28926c0e145b31259
aard313 <39379251+aard313@users.noreply.github.com>
2020-10-15 21:56:15 +0200

    edited snowbot
    added a=variable in snowboydecoder in the start function, within the loop of the  start function a code is added to keep up the silence time since laast detection. if the max time is reached the added variable is called.
    in the volume_+_demo_2 file cleaned up the code and added the extra functionality the new decoder offers

--- volume_+_demo_2.py
+++ volume_+_demo_2.py
@@ -2,33 +2,29 @@
 import os
 import alsaaudio
 from RPi import GPIO
-from time import sleep
+import time
+import snowboydetect
 import snowboydecoder
 import sys
 import signal
 
-os.system('clear') #clear screen, this is just for the OCD purposes
+
+#os.system('clear') #clear screen, this is just for the OCD purposes
  
 #setup mixer module
 m = alsaaudio.Mixer(control='Speaker',cardindex =2)
 
 #capture SIGINT signal, e.g., Ctrl + C
-signal.signal(signal.SIGINT, signal_handler)
-
 
 #Constants and variables
+TURN_OFF_TIME = 11000
 MAX_VOLUME = 100 #set the maximum volume
 DIRECTION  = -1   #set the direction to increase volume 1 or -1
 STEP = 5 #linear steps for increasing/decreasing volume
 amplifying = False #amplifying state
 interrupted = False #intterrupted variable
-
-#get initial values
-volume = int(m.getvolume()[0])  #volume of the speakers
-clkLastState = GPIO.input(clk)  #Laststate of the clk pin 
-dtLastState = GPIO.input(dt)    #Laststate of the dt pin 
-swLastState = GPIO.input(sw)    #Laststate of the sw pin
-
+prev_sound = int(round(time.time() * 1000)) #get the current time 
+prev_time = int(round(time.time() * 1000)) #get current time
 
 #tell to GPIO library to use logical PIN names/numbers, instead of the physical PIN numbers
 GPIO.setmode(GPIO.BCM) 
@@ -37,10 +33,9 @@ GPIO.setmode(GPIO.BCM)
 clk = 17
 dt = 18
 sw = 27
-red = 23
+red = 24
 green = 22
-blue = 24
-
+blue = 23
 
 #set up the GPIO events on those pins
 GPIO.setup(red, GPIO.OUT)
@@ -49,7 +44,31 @@ GPIO.setup(blue, GPIO.OUT)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.output(red, GPIO.LOW)
+GPIO.output(green, GPIO.LOW)
+GPIO.output(blue, GPIO.HIGH)
+time.sleep(1)
+
+#get initial values
+volume = 50  #volume of the speakers
+clkLastState = GPIO.input(clk)  #Laststate of the clk pin 
+dtLastState = GPIO.input(dt)    #Laststate of the dt pin 
+swLastState = GPIO.input(sw)    #Laststate of the sw pin
+
+
+### demo.py definitions
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
  
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
 #load the model en setup the detector for snowboy
 model = "/home/pi/Desktop/project/hello.pmdl"
 detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5,audio_gain=3)
@@ -66,7 +85,7 @@ def clkClicked(channel):
 
     clkState = GPIO.input(clk)
     dtState = GPIO.input(dt)
-    if amplifying == False:
+    if amplifying == True:
         if clkState == 0 and dtState == 1:
             volume = volume + (STEP * DIRECTION)
             if volume > MAX_VOLUME:
@@ -85,7 +104,7 @@ def dtClicked(channel):
     
     clkState = GPIO.input(clk)
     dtState = GPIO.input(dt)
-    if amplifying == False: 
+    if amplifying == True: 
         if clkState == 1 and dtState == 0:
             volume = volume - (STEP * DIRECTION)
             if volume > MAX_VOLUME:
@@ -102,22 +121,16 @@ def swClicked(channel):
     
     if amplifying:
         amplifying = False
-        m.setvolume(int(volume))
+        m.setvolume(0)
+        led_light(red)
     else:
         amplifying = True
-        m.setvolume(0)
+        m.setvolume(volume)
+        led_light(green)
     
     print ("amplifying ", amplifying)             
 
 
-### demo.py definitions
-def signal_handler(signal, frame):
-    global interrupted
-    interrupted = True
- 
-def interrupt_callback():
-    global interrupted
-    return interrupted
 
 def led_light(color):
     if color == green:
@@ -134,10 +147,22 @@ def led_light(color):
         GPIO.output(blue,GPIO.LOW)
 
 def turn_on():
+    global volume
+    global amplifying
+    global prev_sound
+    m.setvolume(int(volume))
     amplifying = True
     led_light(green)
+    
+def turn_off():
+    global amplifying
+    m.setvolume(0)
+    amplifying = False
+    led_light(red)
 
 
+
+    
 
 #set up the interrupts
 GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=50)
@@ -154,23 +179,15 @@ print('Listening... Press Ctrl + C to exit')
 
 
 
-detector.start(detected_callback=turn_on,
+detector.start(detected_callback=turn_on, stop_amplifying=turn_off,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
-
-while amplifying:
-    status = detector.RunDetection() 
-    if status == -2:
-        swClicked()
-        led_light(red)   
-    elif status == 0:
-        led_light(green)
-    sleep(.1)
 
 
 ### terminators
 detector.terminate()
 GPIO.cleanup()
+
 
 
