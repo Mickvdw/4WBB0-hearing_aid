6054091d629bcb6bbd3bc164c0fd03f39c97bccd
Mickvdw <57793487+Mickvdw@users.noreply.github.com>
2020-10-15 16:01:57 +0200

    Update pyo_start_voiceback.py

--- pyo_start_voiceback.py
+++ pyo_start_voiceback.py
@@ -1,6 +1,9 @@
 import pyo
-
+from pyo import Tone
+from pyo import Atone
 
 s=pyo.Server().boot()
 s.start()
-i = pyo.Input().out()
+i = pyo.Input()
+low_filter = Tone(i, freq=280, mul=1, add=0)
+high_filter = Atone(low_filter, freq=4000, mul=1, add=0).out()
